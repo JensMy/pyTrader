c45e23c02760c9a864368fabac9b915c0c88315f
JensMy <jens.mysliwiec1@gmail.com>
2019-12-29 16:22:03 +0100

    Add files via upload

--- aindexer.py
+++ aindexer.py
@@ -1,19 +1,21 @@
 import datetime as dt
 import os
-import numpy as np
 import pickle
-import pandas as pd
-import bs4 as bs
-import pandas_datareader.data as web
-from pandas_datareader._utils import RemoteDataError
-import matplotlib.pyplot as plt
-from matplotlib import style
-import requests
 
-style.use('ggplot')
+import bs4 as bs
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import pandas_datareader.data as web
+import requests
+from matplotlib import style
+from pandas_datareader._utils import RemoteDataError
+
+style.use ('ggplot')
 
 
 def safe_dax_tickers(workdir, aindex, wikilink, t_col):
+    # extrahiert Datensatz aus Wikipedia
 
     resp = requests.get (wikilink)
     soup = bs.BeautifulSoup (resp.text, 'lxml')
@@ -47,6 +49,7 @@ def safe_dax_tickers(workdir, aindex, wikilink, t_col):
 
 
 def get_data_from_yahoo(workdir, aindex, start, end, reload_dax=False):
+    # lädt Umfangreiche Datebsätze als csv von Yahoo Finance herunter
     if reload_dax:
         tickers = safe_aindex_tickers ()
     else:
@@ -85,19 +88,19 @@ def get_data_from_yahoo(workdir, aindex, start, end, reload_dax=False):
 #get_data_from_yahoo ()
 
 def compile_data(workdir, aindex):
-    with open (workdir + aindex +'tickers.pickle', 'rb') as f:
+    # extrahiert aus den Datensätzen die Adj Close Spalte
+
+    with open (workdir + aindex + 'tickers.pickle', 'rb') as f:
         tickers = pickle.load (f)
 
     for count, ticker in enumerate (tickers):
-
-        main_df = pd.DataFrame()
-
+        main_df = pd.DataFrame ()
 
     for count, ticker in enumerate (tickers):
 
         df = pd.read_csv (workdir + 'stock_dfs/{}.csv'.format (ticker))
         df.set_index ('Date', inplace=True)
-        df.rename(columns={'Adj Close': ticker}, inplace=True)
+        df.rename (columns={'Adj Close': ticker}, inplace=True)
 
 
         df.drop (['Open', 'High', 'Low', 'Close', 'Volume','50ma'], 1, inplace=True)
@@ -117,25 +120,27 @@ def compile_data(workdir, aindex):
 #compile_data ()
 
 def visualize_data(workdir, aindex):
-    df= pd.read_csv(workdir + aindex+'_joined_closes.csv')
+    # Ausgabe auf einer Heatmap
 
-    df_corr = df.corr()
+    df = pd.read_csv (workdir + aindex + '_joined_closes.csv')
+
+    df_corr = df.corr ()
 
     data = df_corr.values
-    fig = plt.figure()
-    ax = fig.add_subplot(1,1,1)
-    heatmap = ax.pcolor(data,cmap=plt.cm.RdYlGn)
-    fig.colorbar(heatmap)
-    ax.set_xticks(np.arange(data.shape[0]) + 0.5, minor=False)
-    ax.set_yticks(np.arange(data.shape[1]) + 0.5, minor=False)
+    fig = plt.figure ()
+    ax = fig.add_subplot (1, 1, 1)
+    heatmap = ax.pcolor (data, cmap=plt.cm.RdYlGn)
+    fig.colorbar (heatmap)
+    ax.set_xticks (np.arange (data.shape[0]) + 0.5, minor=False)
+    ax.set_yticks (np.arange (data.shape[1]) + 0.5, minor=False)
 
-    ax.invert_yaxis()
-    ax.xaxis.tick_top()
+    ax.invert_yaxis ()
+    ax.xaxis.tick_top ()
 
     column_lables = df_corr.columns
     row_lables = df_corr.index
-    ax.set_xticklabels(column_lables)
-    ax.set_yticklabels(row_lables)
+    ax.set_xticklabels (column_lables)
+    ax.set_yticklabels (row_lables)
     plt.xticks(rotation=90)
     heatmap.set_clim(-1,1)
     plt.tight_layout()
@@ -147,21 +152,14 @@ def visualize_data(workdir, aindex):
 #visualize_data()
 
 
-
-
-
-
-
-def main(aindex,wikilink,t_col):
-    workdir = "Files/" + aindex + "/"
+def main(workdir, aindex, wikilink, t_col, v):
+    workdir = workdir + "/Files/" + aindex + "/"
     start = dt.datetime (1940, 1, 1)
     end = dt.datetime (2019, 12, 19)
-    if not os.path.exists (workdir+aindex+"/"):
-        os.makedirs (workdir + aindex+ '/')
-    safe_dax_tickers (workdir,aindex=aindex,wikilink=wikilink,t_col=t_col)
+    if not os.path.exists (workdir + aindex + "/"):
+        os.makedirs (workdir + aindex + '/')
+    safe_dax_tickers (workdir, aindex=aindex, wikilink=wikilink, t_col=t_col)
     get_data_from_yahoo (workdir=workdir, start=start, end=end, aindex=aindex)
-    compile_data(workdir,aindex)
-    visualize_data (workdir,aindex)
-
-
-
+    compile_data (workdir, aindex)
+    if v == True:
+        visualize_data (workdir, aindex)
--- launcher.py
+++ launcher.py
@@ -1,9 +1,22 @@
 import aindexer
+
+
+# Main Methode zum Aufruf der Aindexer Funktion
+
 def launch():
-    #aindexer.main(aindex ="dax",wikilink ="https://de.wikipedia.org/wiki/DAX",t_col = 1)
-    #aindexer.main (aindex="sp500", wikilink="https://en.wikipedia.org/wiki/List_of_S%26P_500_companies", t_col=0)
-    #aindexer.main(aindex ="dow",wikilink ="https://en.wikipedia.org/wiki/Dow_Jones_Industrial_Average#Components",t_col = 2)
-    aindexer.main (aindex="krypto", wikilink="https://de.wikipedia.org/wiki/Liste_von_Kryptow%C3%A4hrungen",t_col=2)
+    # working
+    workdir = ".."
+    aindexer.main (workdir=workdir, aindex="dax", wikilink="https://de.wikipedia.org/wiki/DAX", t_col=1, v=False)
+    # working
+    aindexer.main (workdir=workdir, aindex="sp500",
+                   wikilink="https://en.wikipedia.org/wiki/List_of_S%26P_500_companies", t_col=0, v=False)
+    # need to Filter out EXCHANGE Listing
+    aindexer.main (workdir=workdir, aindex="dow",
+                   wikilink="https://en.wikipedia.org/wiki/Dow_Jones_Industrial_Average#Components",
+                   t_col=2, v=False)
+    # need to Seperate BTC/XBT into 2 seperate Tickers
+    aindexer.main (workdir=workdir, aindex="krypto",
+                   wikilink="https://de.wikipedia.org/wiki/Liste_von_Kryptow%C3%A4hrungen", t_col=2, v=False)
 
 
-launch()
+launch ()
